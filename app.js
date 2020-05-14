6172844e4c46382bc1ca713b691d165e1c566ba8
NitinNanda <nitinnanda11@gmail.com>
2020-05-13 23:24:05 -0400

    Implemented the roll dice and hold score functionality. The active player will be toggled as per the game logic

--- app.js
+++ app.js
@@ -9,22 +9,73 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random()*6) + 1;
 
-document.querySelector('#current-'+activePlayer).textContent = dice;
+// document.querySelector('#current-'+activePlayer).textContent = dice;
 // document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+dice+'</em>';
 
-var x  = document.querySelector('#score-0').textContent;
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    // 1. Random number
+    var dice = Math.floor(Math.random()*6) + 1;
+
+    // 2. display the results
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score if rolled number was 1
+    if(dice !== 1){
+        // add score
+        roundScore += dice;
+        document.querySelector('#current-'+activePlayer).textContent = roundScore;
+    }else{
+        // next player
+        nextPlayer();
+    }
+});
 
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // Add current score to global Score
+    scores[activePlayer] += roundScore;
+
+    // update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if the player won the game
+    if(scores[activePlayer] >= 20){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner !'
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+    }else{
+        nextPlayer();
+    }
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+}
 
 
 
